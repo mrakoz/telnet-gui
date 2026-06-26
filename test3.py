e410da8289f36262c63f36781fb953392f7de55c
mrakoz <59206054+mrakoz@users.noreply.github.com>
2026-06-26 22:38:02 +0200

    Remove orphaned test3.py (debug leftover)

--- test3.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-app = QApplication(sys.argv)
-w = QMainWindow()
-w.show()
-print("Visible?", w.isVisible())
-ret = app.exec_()
-print("Exec returned", ret)
